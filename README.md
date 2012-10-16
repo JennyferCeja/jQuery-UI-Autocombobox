9a87ff2d8e6f3cc21da90fd41afd7b598901fb93
Sébastien Gissinger <sgissinger@gmail.com>
2012-10-16 10:20:48 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
 jQuery-UI-Autocombobox
 ======================
 
-A jQuery-UI Autocomplete field using a &lt;select> HTML element as source of text and value. Forked from http://www.learningjquery.com/2010/06/a-jquery-ui-combobox-under-the-hood
+This project is a jQuery-UI Autocomplete field using the &lt;option>&lt;/option> elements of a &lt;select>&lt;/select> element as the source of text for the autocomplete and value.
+
+It permits autocompletion on textual values within the &lt;option>&lt;/option> elements while using &lt;option>&lt;/option> "val" attribute as the real value of the autocomplete combobox
+
+Original &lt;select>&lt;/select> element is also kept up-to-date by the autocomplete combobox and must contains at least an &lt;option val="">&lt;/option> element which will be used when no value matches the searched text
+
+## Usage
+
+
+This creates the autocomplete combobox
+
+    jQuery('#mySelect').autocombobox();
+
+This sets the value of the autocomplete combobox and of the underlying combobox. This value must be one the &lt;option>&lt;/option> val attribute otherwise it will be set to ''
+
+    jQuery('#mySelect').autocombobox('setValue', value);
+
+This returns the value of the selected &lt;option>&lt;/option> element
+
+    jQuery('#mySelect').val();
+    jQuery('#mySelect').autocombobox().val();
+
+    
+## Compatibility
+
+Works well on
+
+ * Google Chrome 22.0
+ * IE9
+ * IE9 in IE8 mode
+ * Firefox 15
+ * Safari 5.1
+
+Works with some glitches on
+
+ * IE9 in IE7 mode 
+ * IE6
+
+It's forked from http://www.learningjquery.com/2010/06/a-jquery-ui-combobox-under-the-hood
---
+++ jquery.ui.autocomplete.combobox.js
@@ -0,0 +1,139 @@
+jQuery.widget('ui.autocombobox', {
+    input : null,
+    select : null,
+    _create : function () {
+        var input;
+        var self = this,
+        select = this.select = this.element.hide(),
+        selected = select.children(':selected'),
+        value = selected.val() ? selected.text() : '',
+        wrapper = jQuery('<span>').addClass('ui-autocombocontainer')
+                                  .insertAfter(select);
+
+        input = this.input = jQuery('<input>')
+            .appendTo(wrapper)
+            .addClass('ui-autocombobox')
+            .val(value)
+            .autocomplete({
+                delay : 0,
+                minLength : 0,
+                position: {
+                    my: 'left top',
+                    at: 'left bottom',
+                    collision: 'flip',
+                    of: wrapper
+                },
+                source : function (request, response) {
+                    var matcher = new RegExp(jQuery.ui.autocomplete.escapeRegex(request.term), 'i');
+                    response(select.children('option').map(function () {
+                            var text = jQuery(this).text();
+                            if (this.value && (!request.term || matcher.test(text)))
+                                return {
+                                    label : text.replace(
+                                        new RegExp(
+                                            '(?![^&;]+;)(?!<[^<>]*)(' +
+                                            jQuery.ui.autocomplete.escapeRegex(request.term) +
+                                            ')(?![^<>]*>)(?![^&;]+;)', 'gi'), '<b>$1</b>'),
+                                    value : text,
+                                    option : this
+                                };
+                        }));
+                },
+                select : function (event, ui) {
+                    input.autocomplete('close');
+                    ui.item.option.selected = true;
+                    self._trigger('selected', event, {
+                        item : ui.item.option
+                    });
+                },
+                change : function (event, ui) {
+                    if (!ui.item) {
+                        var matcher = new RegExp('^' + jQuery.ui.autocomplete.escapeRegex(jQuery(this).val()) + '$', 'i'),
+                        valid = false;
+                        select.children('option').each(function () {
+                            if (jQuery(this).text().match(matcher)) {
+                                this.selected = valid = true;
+                                return false;
+                            }
+                        });
+                        if (!valid) {
+                            // remove invalid value, as it didn't match anything
+                            jQuery(this).val('');
+                            select.val('');
+                            input.data('autocomplete').term = '';
+                            return false;
+                        }
+                    }
+                }
+            })
+            .dblclick(function()
+            {
+                input.autocomplete('search', '');
+            });
+
+        input.data('autocomplete')._renderItem = function (ul, item)
+        {
+            if(!ul.hasClass('ui-autocombolist'))
+                ul.addClass('ui-autocombolist');
+
+            return jQuery('<li></li>').data('item.autocomplete', item)
+                                      .append('<a>' + item.label + '</a>')
+                                      .appendTo(ul);
+        };
+
+        input.width(input.width() - 36);
+
+        jQuery('<button> </button>').attr('tabIndex', -1)
+                                    .attr('title', 'Voir tous les éléments')
+                                    .addClass('ui-autocombobutton')
+                                    .insertAfter(input)
+                                    .button({
+                                        icons : {
+                                            primary : 'ui-icon-triangle-1-s'
+                                        },
+                                        text : false
+                                    })
+                                    .removeClass('ui-corner-all')
+                                    .addClass('ui-corner-right')
+                                    .click(function () {
+                                        // close if already visible
+                                        if (input.autocomplete('widget').is(':visible')) {
+                                            input.autocomplete('close');
+                                            return;
+                                        }
+                                        // pass empty string as value to search for, displaying all results
+                                        input.autocomplete('search', '');
+                                        input.focus();
+                                    });
+    },
+    setValue : function(val) {
+        this.select.val(val);
+        text = this.select.children(':selected').text();
+        
+        var matcher = new RegExp('^' + jQuery.ui.autocomplete.escapeRegex(text) + '$', 'i'),
+        valid = false;
+        this.select.children('option').each(function () {
+            if (jQuery(this).text().match(matcher)) {
+                this.selected = valid = true;
+                return false;
+            }
+        });
+        if (!valid) {
+            // remove invalid value, as it didn't match anything
+            jQuery(this).val('');
+            this.select.val('');
+            this.input.data('autocomplete').term = '';
+            return false;
+        }
+        else
+        {
+            this.input.val(text);
+            this.input.data('autocomplete').term = text;
+        }
+    },
+    destroy : function () {
+        this.wrapper.remove();
+        this.element.show();
+        jQuery.Widget.prototype.destroy.call(this);
+    }
+});
